ea13825f57bf7c153b26fb9ae526ff84affb28a9
guhan-balaji <guhanbalaji7@gmail.com>
2025-01-18 17:49:51 -0500

    read header

--- app/server.go
+++ app/server.go
@@ -7,9 +7,15 @@ import (
         "strings"
 )
 
-type RequestLine struct {
+type Request struct {
+        // request line
         verb, target, protocol string
+
+        // headers
+        host, userAgent, accept string
+
 }
+
 
 func main() {
         l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -26,34 +32,75 @@ func main() {
                         fmt.Println("Error accepting connection: ", err.Error())
                         os.Exit(1)
                 }
-                b := make([]byte, 1024)
-                _, err = conn.Read(b)
+                defer conn.Close()
+
+                request := make([]byte, 1024)
+                _, err = conn.Read(request)
                 if err != nil {
                         fmt.Println("Error reading connection: ", err.Error())
                         os.Exit(1)
                 }
 
-                rl := extractRequestLine(string(b))
-                if rl.verb == "GET" && strings.HasPrefix(rl.target, "/echo/") {
-
-                        cl := len(rl.target) - len("/echo/")
-                        body := rl.target[len("/echo/"):]
-                        response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", cl, body)
-                        conn.Write([]byte(response))
-
-                } else if rl.verb == "GET" && rl.target == "/" {
-                        conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
-                } else {
-                        conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-                }
+                req := deserializeHttpRequest(request)
+                fmt.Println(req.verb)
+                fmt.Println(req.target)
+                fmt.Println(req.protocol)
+                fmt.Println(req.host)
+                fmt.Println(req.userAgent)
+                fmt.Println(req.accept)
+                sendResponse(req, conn)
                 conn.Close()
         }
 }
 
-func extractRequestLine(request string) RequestLine {
-        req := strings.Split(request, "\r\n")
-        requestLine := req[0]
-        rl := strings.Split(requestLine, " ")
-        return RequestLine{rl[0], rl[1], rl[2]}
+func deserializeHttpRequest(request []byte) *Request {
+        reqParts := strings.Split(string(request), "\r\n")
+        fmt.Println(reqParts)
+
+        req := new(Request)
+
+        reqLine := strings.Split(reqParts[0], " ")
+        req.verb     = reqLine[0]
+        req.target   = reqLine[1]
+        req.protocol = reqLine[2]
+
+
+        for _, rp := range reqParts[1:] {
+                if strings.HasPrefix(rp, "Host") {
+                        req.host = rp[len("Host: "):]
+                } else if strings.HasPrefix(rp, "User-Agent") {
+                        req.userAgent = rp[len("User-Agent: "):]
+                } else if strings.HasPrefix(rp, "Accept") {
+                        req.accept = rp[len("Accept: "):]
+                }
+        }
+        return req
+}
+
+func sendResponse(req *Request, conn net.Conn) {
+
+        if req.verb == "GET" && req.target == "/" {
+
+                conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+        } else if req.verb == "GET" && strings.HasPrefix(req.target, "/echo/") {
+
+                conn.Write([]byte(
+                        fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+                        len(req.target) - len("/echo/"),
+                        req.target[len("/echo/"):],
+                )))
+
+        } else if req.verb == "GET" && strings.HasPrefix(req.target, "/user-agent") {
+
+                conn.Write([]byte(
+                        fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+                        len(req.userAgent),
+                        req.userAgent,
+                )))
+
+        } else {
+
+                conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+        }
 }
